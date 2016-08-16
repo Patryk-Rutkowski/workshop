d4c693324f30db903a0e6d93b956010023719f20
Patryk Rutkowski <Patryk_Rutkowski@rossmann.com.pl>
2016-08-16 15:40:34 +0200

    Resolvers

---
+++ Resolvers/ICarServiceResolver.cs
@@ -0,0 +1,20 @@
+﻿using Database;
+using WorkshopServices;
+using WorkshopServices.Implementation;
+
+namespace Resolvers
+{
+    public static class ICarServiceResolver
+    {
+
+        public static ICarService Get()
+        {
+            var repository = new CarRepository();
+
+            return new CarService(
+                repository
+                );
+        }
+
+    }
+}
